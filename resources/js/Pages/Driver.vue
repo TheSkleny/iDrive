91fbc966699ca40fe704327b1d0c77fc4027a9d3
Adam Světlík <adam.m.svetlik@gmail.com>
2023-11-15 22:39:33 +0100

    init driver view

---
+++ resources/js/Pages/Driver.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import Base from '@/Pages/Base/App.vue'
+
+
+</script>
+
+<template>
+    <Base>
+        <div>
+            <h1>Driver</h1>
+            <v-btn>Refresh</v-btn>
+            <ul>
+                <li v-for=""
+            </ul>
+        </div>
+    </Base>
+</template>
+
+<style scoped>
+
+</style>
