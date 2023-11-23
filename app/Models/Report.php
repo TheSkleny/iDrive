1d4d56a4e7074941776114ffebf9a24cfdc2c11f
David Sklenar <dav.sklen@gmail.com>
2023-11-23 18:07:56 +0100

    Models cast enums

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\ReportStateEnum;
+use App\Enums\ReportTypeEnum;
 
 class Report extends Model
 {
@@ -32,6 +34,18 @@ class Report extends Model
         'MaintenanceDate',
         'StateId',
         'TypeId'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'ReportDate' => 'date',
+        'MaintenanceDate' => 'date',
+        'StateId' => ReportStateEnum::class,
+        'TypeId' => ReportTypeEnum::class,
     ];
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Enums\UserTypeEnum;
 
 
 class User extends Authenticatable
@@ -44,6 +45,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'type_id' => UserTypeEnum::class,
     ];
 
     protected $attributes = [
