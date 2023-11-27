8a87cf87f0af77c6f475c51cdf71241ff9492986
David Sklenar <dav.sklen@gmail.com>
2023-11-27 09:19:06 +0100

    hroku goes brrrrrrr

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -23,7 +23,7 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.post(route('login'), {
+    form.post(route('login', {}, { protocol: 'https' }), {
         onFinish: () => form.reset('password'),
     });
 };
